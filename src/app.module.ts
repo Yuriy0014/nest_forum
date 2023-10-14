fb968ffbdb94a2e8c89075ca807f0b97ae3fef6d
YurL0014 <ylogachev-sfedu@mail.ru>
2023-10-14 12:24:49 +0300

    Created classes and added it in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BlogsController } from './features/blogs/blogs.controller';
+import { BlogsService } from './features/blogs/blogs.service';
+import { BlogsRepo } from './features/blogs/blogs.repo';
+import { BlogsQueryRepo } from './features/blogs/blogs.query-repo';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest_forum')],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, BlogsController],
+  providers: [AppService, BlogsService, BlogsRepo, BlogsQueryRepo],
 })
 export class AppModule {}

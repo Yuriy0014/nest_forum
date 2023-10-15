7ba5f280ec678afa40aa65a591d2cb6e6bb930f3
YurL0014 <ylogachev-sfedu@mail.ru>
2023-10-15 19:24:20 +0300

    Added missing collections to deletion

--- src/features/testing/testing.controller.ts
+++ src/features/testing/testing.controller.ts
@@ -14,6 +14,16 @@ import {
   Blog,
   BlogModelType,
 } from '../blogs/models/domain/blogs.domain-entities';
+import {
+  User,
+  UserModelType,
+} from '../users/models/domain/users.domain-entities';
+import {
+  Like,
+  LikeModelType,
+  UsersLikesConnection,
+  UsersLikesConnectionType,
+} from '../likes/models/domain/likes.domain-entities';
 
 @Controller('testing')
 export class TestingController {
@@ -22,6 +32,12 @@ export class TestingController {
     private readonly blogModel: BlogModelType,
     @InjectModel(Post.name)
     private readonly postModel: postModelType,
+    @InjectModel(User.name)
+    private readonly userModel: UserModelType,
+    @InjectModel(Like.name)
+    private readonly likeModel: LikeModelType,
+    @InjectModel(UsersLikesConnection.name)
+    private readonly usersLikesConnectionModel: UsersLikesConnectionType,
   ) {}
 
   @Delete('all-data')
@@ -30,6 +46,9 @@ export class TestingController {
     await Promise.all([
       this.blogModel.deleteMany({}),
       this.postModel.deleteMany({}),
+      this.userModel.deleteMany({}),
+      this.likeModel.deleteMany({}),
+      this.usersLikesConnectionModel.deleteMany({}),
     ]).catch((e) => {
       console.log(e);
       throw new HttpException('Not Found', HttpStatus.INTERNAL_SERVER_ERROR);

d78d15c42574fea67bc5061d00cb7258605a6d41
YurL0014 <ylogachev-sfedu@mail.ru>
2023-10-15 12:15:55 +0300

    Connected comments controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,17 +17,36 @@ import { PostsRepo } from './features/posts/posts.repo';
 import { PostsQueryRepo } from './features/posts/posts.query-repo';
 import { MapPostViewModel } from './features/posts/helpers/map-PostViewModel';
 import { TestingController } from './features/testing/testing.controller';
+import { CommentsController } from './features/comments/comments.controller';
+import { CommentsService } from './features/comments/comments.service';
+import { CommentsRepo } from './features/comments/comments.repo';
+import { CommentsQueryRepo } from './features/comments/comments.query-repo';
+import { MapCommentViewModel } from './features/comments/helpers/map-CommentViewModel';
+import {
+  Post,
+  PostSchema,
+} from './features/posts/models/domain/posts.domain-entities';
+import {
+  Comment,
+  CommentSchema,
+} from './features/comments/models/domain/comments.domain-entities';
+import { LikesQueryRepo } from './features/likes/likes.query-repo';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest_forum'),
-    MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }]),
+    MongooseModule.forFeature([
+      { name: Blog.name, schema: BlogSchema },
+      { name: Post.name, schema: PostSchema },
+      { name: Comment.name, schema: CommentSchema },
+    ]),
   ],
   controllers: [
     AppController,
     TestingController,
     BlogsController,
     PostsController,
+    CommentsController,
   ],
   providers: [
     AppService,
@@ -41,6 +60,13 @@ import { TestingController } from './features/testing/testing.controller';
     PostsRepo,
     PostsQueryRepo,
     MapPostViewModel,
+    // Comments
+    CommentsService,
+    CommentsRepo,
+    CommentsQueryRepo,
+    MapCommentViewModel,
+    // Likes
+    LikesQueryRepo,
   ],
 })
 export class AppModule {}

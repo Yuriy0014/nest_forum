7e8476b998f953ab140f20196dfdb57342f8a5ad
YurL0014 <ylogachev-sfedu@mail.ru>
2023-10-15 10:26:38 +0300

    Added posts files to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,13 +10,31 @@ import {
   Blog,
   BlogSchema,
 } from './features/blogs/models/domain/blogs.domain-entities';
+import { MapBlogViewModel } from './features/blogs/helpers/map-BlogViewModel';
+import { PostsController } from './features/posts/posts.controller';
+import { PostsService } from './features/posts/posts.service';
+import { PostsRepo } from './features/posts/posts.repo';
+import { PostsQueryRepo } from './features/posts/posts.query-repo';
+import { MapPostViewModel } from './features/posts/helpers/map-PostViewModel';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest_forum'),
     MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }]),
   ],
-  controllers: [AppController, BlogsController],
-  providers: [AppService, BlogsService, BlogsRepo, BlogsQueryRepo],
+  controllers: [AppController, BlogsController, PostsController],
+  providers: [
+    AppService,
+    // Blogs
+    BlogsService,
+    BlogsRepo,
+    BlogsQueryRepo,
+    MapBlogViewModel,
+    // Posts
+    PostsService,
+    PostsRepo,
+    PostsQueryRepo,
+    MapPostViewModel,
+  ],
 })
 export class AppModule {}

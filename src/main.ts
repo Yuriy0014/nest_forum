c26fe8a72260158ae81f70d4b464421ce549901a
YurL0014 <ylogachev-sfedu@mail.ru>
2023-10-19 00:24:06 +0300

    Basic settings for using Guards and WM

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import process from 'process';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import {
+  ErrorExceptionFilter,
+  HttpExceptionFilter,
+} from './middlewares/exception.filter';
 
 const port = process.env.PORT || 7200;
 
@@ -9,8 +14,31 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(cookieParser());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // Автомаически преобразует входящие данные по типам. Например id из params делает из строки
+      // числом, если указано @Params('id') userId: number
+      transform: true,
+      stopAtFirstError: true,
+      exceptionFactory: (errors) => {
+        const errorsForResponse = [];
+
+        errors.forEach((e) => {
+          const constrKeys = Object.keys(e.constraints!);
+          constrKeys.forEach((ckey) => {
+            // @ts-ignore
+            errorsForResponse.push({
+              message: e.constraints![ckey],
+              field: e.property,
+            });
+          });
+        });
+
+        throw new BadRequestException(errorsForResponse);
+      },
+    }),
+  );
+  app.useGlobalFilters(new ErrorExceptionFilter(), new HttpExceptionFilter());
   await app.listen(port);
 }
 bootstrap();
-
-//Code for Vercel
---
+++ src/middlewares/auth.guard.ts
@@ -0,0 +1,24 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  // constructor(protected userService: UsersService) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    // return validateRequest(request);
+
+    console.log(request.headers.authorization);
+
+    // throw new UnauthorizedException();
+    return false;
+  }
+}

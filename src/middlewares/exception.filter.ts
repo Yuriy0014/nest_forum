edb3467192ba6f82353f6c0db072090adb893e99
YurL0014 <ylogachev-sfedu@mail.ru>
2023-10-19 00:25:12 +0300

    Added exception filter

---
+++ src/middlewares/exception.filter.ts
@@ -0,0 +1,52 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import * as process from 'process';
+
+@Catch(Error)
+export class ErrorExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    if (process.env.environment !== 'production') {
+      response.status(500).json({
+        error: exception.toString(),
+        stack: exception.stack ? exception.stack.toString() : 'Some stack',
+      });
+    } else {
+      response.status(500).send('some error occurred');
+    }
+  }
+}
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+
+    if (status === 400) {
+      const errorResponse = {
+        errors: [],
+      };
+      const responseErr: any = exception.getResponse();
+      // @ts-ignore
+      responseErr.message.forEach((m) => errorResponse.errors.push(m));
+
+      response.status(status).json(errorResponse);
+    } else {
+      response.status(status).json({
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+      });
+    }
+  }
+}

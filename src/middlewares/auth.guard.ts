e2b0ebcc1d14b0dd14faf393c8f1c8c97ce36ed1
YurL0014 <ylogachev-sfedu@mail.ru>
2023-10-19 01:11:31 +0300

    Added ExistingEmail and BaseAuth guards

--- src/middlewares/auth.guard.ts
+++ src/middlewares/auth.guard.ts
@@ -3,22 +3,72 @@ import {
   CanActivate,
   ExecutionContext,
   UnauthorizedException,
+  BadRequestException,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { UsersQueryRepo } from '../features/users/users.query-repo';
+
+// @Injectable()
+// export class AuthGuard implements CanActivate {
+//   // constructor(protected userService: UsersService) {}
+//
+//   canActivate(
+//     context: ExecutionContext,
+//   ): boolean | Promise<boolean> | Observable<boolean> {
+//     const request = context.switchToHttp().getRequest();
+//     // return validateRequest(request);
+//
+//     console.log(request.headers.authorization);
+//
+//     // throw new UnauthorizedException();
+//     return false;
+//   }
+// }
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuardBase implements CanActivate {
   // constructor(protected userService: UsersService) {}
 
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-    // return validateRequest(request);
+    if (request.headers['authorization'] !== 'Basic YWRtaW46cXdlcnR5') {
+      throw new UnauthorizedException([{ message: 'UnauthorizedException' }]);
+    } else {
+      return true;
+    }
+  }
+}
 
-    console.log(request.headers.authorization);
+@Injectable()
+export class ExistingEmailGuard implements CanActivate {
+  constructor(protected usersQueryRepo: UsersQueryRepo) {}
 
-    // throw new UnauthorizedException();
-    return false;
+  async canActivate(
+    context: ExecutionContext,
+    // @ts-ignore
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const req = context.switchToHttp().getRequest();
+
+    const loginExists = await this.usersQueryRepo.findByLoginOrEmail(
+      req.body.login,
+    );
+    const emailExists = await this.usersQueryRepo.findByLoginOrEmail(
+      req.body.email,
+    );
+
+    if (loginExists) {
+      throw new BadRequestException([
+        { message: 'BAD REQUEST', field: 'login' },
+      ]);
+    }
+
+    if (emailExists) {
+      throw new BadRequestException([
+        { message: 'BAD REQUEST', field: 'email' },
+      ]);
+    }
+    return true;
   }
 }

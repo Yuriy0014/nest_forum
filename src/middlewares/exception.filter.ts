3c24cd168eb14d5296e231d8d16dc27b8e00eae4
YurL0014 <ylogachev-sfedu@mail.ru>
2023-10-24 16:10:41 +0300

    Changed logic for general exception output

--- src/middlewares/exception.filter.ts
+++ src/middlewares/exception.filter.ts
@@ -3,24 +3,40 @@ import {
   Catch,
   ArgumentsHost,
   HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 import * as process from 'process';
 
 @Catch(Error)
 export class ErrorExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
+  catch(exception: any, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
 
-    if (process.env.environment !== 'production') {
-      response.status(500).json({
-        error: exception.toString(),
-        stack: exception.stack ? exception.stack.toString() : 'Some stack',
-      });
-    } else {
-      response.status(500).send('some error occurred');
+    const errorResponse = {
+      errorsMessages: [],
+    };
+    const responseErr: any = exception.errors;
+    for (const key in responseErr) {
+      const error = {
+        message: responseErr[key].message,
+        field: key,
+      };
+      // @ts-ignore
+      errorResponse.errorsMessages.push(error);
     }
+    response.status(HttpStatus.BAD_REQUEST).json(errorResponse);
+
+    // В будущем разделить вывод ошибок для теста, и для прода
+    // if (process.env.environment !== 'production') {
+    //   response.status(500).json({
+    //     error: exception.toString(),
+    //     stack: exception.stack ? exception.stack.toString() : 'Some stack',
+    //   });
+    // } else {
+    //   response.status(500).send('some error occurred');
+    // }
   }
 }
 

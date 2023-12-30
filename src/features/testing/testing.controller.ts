e6e4391f5a08e124b0ab5f529069192a05793768
YurL0014 <ylogachev-sfedu@mail.ru>
2023-12-30 14:45:17 +0300

    Логика очистки

--- src/features/testing/testing.controller.ts
+++ src/features/testing/testing.controller.ts
@@ -1,31 +1,35 @@
 import {
-  Controller,
-  Delete,
-  HttpCode,
-  HttpException,
-  HttpStatus,
+    Controller,
+    Delete,
+    HttpCode,
+    HttpException,
+    HttpStatus,
 } from '@nestjs/common';
-import { InjectDataSource } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
+import {InjectDataSource} from '@nestjs/typeorm';
+import {DataSource} from 'typeorm';
 
 @Controller('testing')
 export class TestingController {
-  constructor(@InjectDataSource() protected dataSource: DataSource) {}
+    constructor(@InjectDataSource() protected dataSource: DataSource) {
+    }
 
-  @Delete('all-data')
-  @HttpCode(204)
-  async deleteAll() {
-    await Promise.all([
-      this.dataSource.query(`DELETE FROM public.sessions`),
-      this.dataSource.query(`DELETE FROM public.users`),
-      this.dataSource.query(`DELETE FROM public.blogs`),
-      this.dataSource.query(`DELETE FROM public.posts`),
-      this.dataSource.query(`DELETE FROM public.likes`),
-      this.dataSource.query(`DELETE FROM public.userslikesconnection`),
-      this.dataSource.query(`DELETE FROM public.comments`),
-    ]).catch((e) => {
-      console.log(e);
-      throw new HttpException('Not Found', HttpStatus.INTERNAL_SERVER_ERROR);
-    });
-  }
+    @Delete('all-data')
+    @HttpCode(204)
+    async deleteAll() {
+        await this.dataSource.query(`DELETE
+                                     FROM public.sessions`);
+        await this.dataSource.query(`DELETE
+                                     FROM public.users`);
+        await this.dataSource.query(`DELETE
+                                     FROM public.blogs`);
+        await this.dataSource.query(`DELETE
+                                     FROM public.posts`);
+        await this.dataSource.query(`DELETE
+                                     FROM public.likes`);
+        await this.dataSource.query(`DELETE
+                                     FROM public.userslikesconnection`);
+        await this.dataSource.query(`DELETE
+                                     FROM public.comments`);
+
+    }
 }

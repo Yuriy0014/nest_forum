9dc5f94f8bc82b72c5eaf60b18285ecfd79e98f2
YurL0014 <ylogachev-sfedu@mail.ru>
2024-01-10 12:31:46 +0300

    Style

--- src/features/blogs/helpers/map-BlogViewModelMongo.ts
+++ src/features/blogs/helpers/map-BlogViewModelMongo.ts
@@ -3,14 +3,14 @@ import { BlogDbModel, BlogViewModel } from '../models/blogs.models-mongo';
 
 @Injectable()
 export class MapBlogViewModelMongo {
-  getBlogViewModel = (blog: BlogDbModel): BlogViewModel => {
-    return {
-      id: blog._id.toString(),
-      name: blog.name,
-      description: blog.description,
-      websiteUrl: blog.websiteUrl,
-      createdAt: blog.createdAt,
-      isMembership: blog.isMembership,
+    getBlogViewModel = (blog: BlogDbModel): BlogViewModel => {
+        return { 
+            id: blog._id.toString(),
+            name: blog.name,
+            description: blog.description,
+            websiteUrl: blog.websiteUrl,
+            createdAt: blog.createdAt,
+            isMembership: blog.isMembership,
+        };
     };
-  };
 }
--- src/middlewares/exception.filter.ts
+++ src/middlewares/exception.filter.ts
@@ -1,38 +1,37 @@
 import {
-  ExceptionFilter,
-  Catch,
-  ArgumentsHost,
-  HttpException,
-  HttpStatus,
+    ExceptionFilter,
+    Catch,
+    ArgumentsHost,
+    HttpException,
+    HttpStatus,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
-import * as process from 'process';
 
 @Catch(Error)
 export class ErrorExceptionFilter implements ExceptionFilter {
-  catch(exception: any, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-
-    const errorResponse = {
-      errorsMessages: [],
-    };
-
-    if (typeof exception.message === 'string') {
-      // @ts-ignore
-      errorResponse.errorsMessages.push(exception.message);
-    } else {
-      const responseErr: any = exception.errors;
-      for (const key in responseErr) {
-        const error = {
-          message: responseErr[key].message,
-          field: key,
+    catch(exception: any, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+ 
+        const errorResponse = {
+            errorsMessages: [],
         };
-        // @ts-ignore
-        errorResponse.errorsMessages.push(error);
-      }
-    }
-    response.status(HttpStatus.BAD_REQUEST).json(errorResponse);
+
+        if (typeof exception.message === 'string') {
+            // @ts-ignore
+            errorResponse.errorsMessages.push(exception.message);
+        } else {
+            const responseErr: any = exception.errors;
+            for (const key in responseErr) {
+                const error = {
+                    message: responseErr[key].message,
+                    field: key,
+                };
+                // @ts-ignore
+                errorResponse.errorsMessages.push(error);
+            }
+        }
+        response.status(HttpStatus.BAD_REQUEST).json(errorResponse);
 
     // В будущем разделить вывод ошибок для теста, и для прода
     // if (process.env.environment !== 'production') {
@@ -43,41 +42,41 @@ export class ErrorExceptionFilter implements ExceptionFilter {
     // } else {
     //   response.status(500).send('some error occurred');
     // }
-  }
+    }
 }
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
-    const status = exception.getStatus();
+    catch(exception: HttpException, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        const request = ctx.getRequest<Request>();
+        const status = exception.getStatus();
 
-    if (status === 400) {
-      const errorResponse = {
-        errorsMessages: [],
-      };
-      console.log('filter', exception.getResponse());
-      const responseErr: any = exception.getResponse();
+        if (status === 400) {
+            const errorResponse = {
+                errorsMessages: [],
+            };
+            console.log('filter', exception.getResponse());
+            const responseErr: any = exception.getResponse();
 
-      if (typeof responseErr === 'string') {
-        // @ts-ignore
-        errorResponse.errorsMessages.push(responseErr);
-      } else {
-        responseErr.message.forEach((m) =>
-          // @ts-ignore
-          errorResponse.errorsMessages.push(m),
-        );
-      }
-      response.status(status).json(errorResponse);
-    } else {
-      response.status(status).json({
-        statusCode: status,
-        timestamp: new Date().toISOString(),
-        path: request.url,
-        responseErr: exception.getResponse(),
-      });
+            if (typeof responseErr === 'string') {
+                // @ts-ignore
+                errorResponse.errorsMessages.push(responseErr);
+            } else {
+                responseErr.message.forEach((m) =>
+                // @ts-ignore
+                    errorResponse.errorsMessages.push(m),
+                );
+            }
+            response.status(status).json(errorResponse);
+        } else {
+            response.status(status).json({
+                statusCode: status,
+                timestamp: new Date().toISOString(),
+                path: request.url,
+                responseErr: exception.getResponse(),
+            });
+        }
     }
-  }
 }

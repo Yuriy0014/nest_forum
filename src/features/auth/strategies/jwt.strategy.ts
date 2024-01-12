54880ae9f1ba2048ba98625aa429a32e00af3070
YurL0014 <ylogachev-sfedu@mail.ru>
2024-01-13 01:09:23 +0300

    style

--- src/features/auth/strategies/jwt.strategy.ts
+++ src/features/auth/strategies/jwt.strategy.ts
@@ -5,15 +5,15 @@ import process from 'process';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
-    super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
-    });
-  }
+    constructor() {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: process.env.JWT_SECRET,
+        });
+    }
 
-  async validate(payload: any) {
-    return { userId: payload.userId };
-  }
-}
+    async validate(payload: any) {
+        return { userId: payload.userId };
+    }
+} 

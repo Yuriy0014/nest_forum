23ccc3927feb235a9698931126652b5752ddb417
YurL0014 <ylogachev-sfedu@mail.ru>
2024-01-13 01:07:22 +0300

    feat(pairquizgame): added initial endpints (mock)

--- src/features/pairquizgame/pairquizgame.controller.ts
+++ src/features/pairquizgame/pairquizgame.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get, Post, UseGuards} from '@nestjs/common';
+import {JwtAuthGuard} from "../auth/guards/jwt-auth.guard";
+import {PairQuizQueryRepoSQL} from "./pairquizgame.query-repo";
 
-@Controller('pairquizgame')
-export class PairQuizGameController {}
+@UseGuards(JwtAuthGuard)
+@Controller('pair-game-quiz/pairs')
+export class PairQuizGameController {
+    constructor(
+        private readonly pairQuizQueryRepoSQL: PairQuizQueryRepoSQL
+    ) {
+    }
+
+    @Get('my-current')
+    async getCurrentPair(){
+        return true
+    }
+
+    @Get(':id')
+    async getPairById(){
+        return true
+    }
+
+    @Post('connection')
+    async connectToPair(){
+        return true
+    }
+
+    @Post('my-current/answers')
+    async sendAnswer(){
+        return true
+    }
+
+
+}

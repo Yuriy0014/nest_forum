9f1dd356cb470702f6bd292d9f8369ec76e04c6e
YurL0014 <ylogachev-sfedu@mail.ru>
2024-01-15 08:11:16 +0300

    refactor(quizquestions): added validation

--- src/features/quizquestions/dto/question.dto.ts
+++ src/features/quizquestions/dto/question.dto.ts
@@ -1,13 +1,37 @@
+import {IsBoolean, IsNotEmpty, IsString, Length, Matches} from "class-validator";
+
+export class inputQuestionCreateDTO {
+    @IsString()
+    @Length(10, 500)
+    @IsNotEmpty()
+    @Matches(/.*\S+.*/, {
+        message: 'name should not consist of whitespace characters',
+    })
+        body: string;
+    
+    correctAnswers: string[]
+}
+
 export class QuestionCreateDTO {
     body: string
     correctAnswers: string[]
+    published: boolean
+    createdAt: Date
 }
 
-export class QuestionUpdateDTO {
-    body: string
+export class inputQuestionUpdateDTO {
+    @IsString()
+    @Length(10, 500)
+    @IsNotEmpty()
+    @Matches(/.*\S+.*/, {
+        message: 'name should not consist of whitespace characters',
+    })
+        body: string;
+
     correctAnswers: string[]
 }
 
-export class QuestionPublishStatusDTO {
-    published: boolean
+export class inputQuestionPublishStatusDTO {
+    @IsBoolean()
+        published: boolean
 }
